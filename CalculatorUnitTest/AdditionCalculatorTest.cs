e62f8cd8af9ab41e23aa7c421f4e87c17f4d60e7
james-yeap <james.yeap@iwowsoft.com.my>
2020-10-28 10:24:06 +0800

    Unit Test fixing

--- CalculatorUnitTest/AdditionCalculatorTest.cs
+++ CalculatorUnitTest/AdditionCalculatorTest.cs
@@ -6,10 +6,12 @@ namespace CalculatorUnitTest
     [TestFixture]
     public class AdditionCalculatorTest
     {
+        Calculate _Calculate;
         [Test]
         public void SumOf1And2EqualTo3()
         {
-            var result1 = Calculator.DoOperation(1, 2, "a");
+            _Calculate = new Calculate();
+            var result1 = _Calculate?.Add(1, 2);
             Assert.AreEqual(3, result1);
         }
     }
@@ -17,18 +19,20 @@ namespace CalculatorUnitTest
     [TestFixture]
     public class SubtractionCalculatorTest
     {
-
+        Calculate _Calculate;
         [Test]
         public void DifferenceOf10And2EqualTo8()
         {
-            var result1 = Calculator.DoOperation(10, 2, "s");
+            _Calculate = new Calculate();
+            var result1 = _Calculate?.Subtract(10, 2);
             Assert.AreEqual(8, result1);
         }
 
         [Test]
         public void DifferenceOf2And10EqualTo8()
         {
-            var result1 = Calculator.DoOperation(2, 10, "s");
+            _Calculate = new Calculate();
+            var result1 = _Calculate?.Subtract(2, 10);
             Assert.AreEqual(-8, result1);
         }
     }
@@ -36,11 +40,12 @@ namespace CalculatorUnitTest
     [TestFixture]
     public class MultiplicationCalculatorTest
     {
-
+        Calculate _Calculate;
         [Test]
         public void MultiplicationOf10And2EqualTo20()
         {
-            var result1 = Calculator.DoOperation(10, 2, "m");
+            _Calculate = new Calculate();
+            var result1 = _Calculate?.Multiply(10, 2);
             Assert.AreEqual(20, result1);
         }
     }
@@ -48,11 +53,12 @@ namespace CalculatorUnitTest
     [TestFixture]
     public class DivisionCalculatorTest
     {
-
+        Calculate _Calculate;
         [Test]
         public void DivisionOf10And2EqualTo5()
         {
-            var result1 = Calculator.DoOperation(10, 2, "d");
+            _Calculate = new Calculate();
+            var result1 = _Calculate?.Divide(10, 2);
             Assert.AreEqual(5, result1);
         }
     }
